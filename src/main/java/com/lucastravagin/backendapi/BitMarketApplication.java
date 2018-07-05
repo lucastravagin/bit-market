a9eeffcf0775bd1cf661dd80e9fbcc12bb7a9ddc
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-07-05 19:15:08 -0300

    Populando o banco de dados toda vez que iniciar a aplicação

--- src/main/java/com/lucastravagin/backendapi/BitMarketApplication.java
+++ src/main/java/com/lucastravagin/backendapi/BitMarketApplication.java
@@ -1,12 +1,30 @@
 package com.lucastravagin.backendapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.lucastravagin.backendapi.domain.Categoria;
+import com.lucastravagin.backendapi.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class BitMarketApplication {
+public class BitMarketApplication implements CommandLineRunner  {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BitMarketApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Alimentos");
+		Categoria cat2 = new Categoria(null, "Saúde");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
