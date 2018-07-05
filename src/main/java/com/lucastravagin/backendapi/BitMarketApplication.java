926a2a981bbb7ac3ad7344ee63e838eb06a9f207
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-07-05 14:00:48 -0300

    Primeiro Commit

---
+++ src/main/java/com/lucastravagin/backendapi/BitMarketApplication.java
@@ -0,0 +1,12 @@
+package com.lucastravagin.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitMarketApplication.class, args);
+	}
+}
