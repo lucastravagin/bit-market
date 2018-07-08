6c96dd8621a6186d52f7dee20308809270d0394b
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-07-08 13:28:39 -0300

    Associação de muitos para muitos com a classe Produtos e Categoria

--- src/main/java/com/lucastravagin/backendapi/BitMarketApplication.java
+++ src/main/java/com/lucastravagin/backendapi/BitMarketApplication.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.lucastravagin.backendapi.domain.Categoria;
+import com.lucastravagin.backendapi.domain.Produto;
 import com.lucastravagin.backendapi.repositories.CategoriaRepository;
+import com.lucastravagin.backendapi.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class BitMarketApplication implements CommandLineRunner  {
+public class BitMarketApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository  produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BitMarketApplication.class, args);
@@ -21,10 +26,21 @@ public class BitMarketApplication implements CommandLineRunner  {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Alimentos");
-		Categoria cat2 = new Categoria(null, "Saúde");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2.55);
+		Produto p2 = new Produto(null, "Impressora", 3.99);
+		Produto p3 = new Produto(null, "Mouse", 2.89);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
--- src/main/java/com/lucastravagin/backendapi/domain/Produto.java
+++ src/main/java/com/lucastravagin/backendapi/domain/Produto.java
@@ -8,33 +8,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-//Esta classe implementa a interface Serializable, os objetos dela poderão ser convertidos para um sequencia de Bytes.
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	// Atributos Básicos
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
-	// Método Construtor
-	public Categoria() {
+	public Produto() {
 	}
 
-	// Contrutor para os atributos
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
-	// Getters e Setters (Encapsulamento)
 	public Integer getId() {
 		return id;
 	}
@@ -51,12 +53,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -67,7 +77,6 @@ public class Categoria implements Serializable {
 		return result;
 	}
 
-	// hashCode (Compara os objetos pelo conteúdo)
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -76,7 +85,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
